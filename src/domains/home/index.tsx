8a7acc5f52d1252432771c210bd2d0f8b13e4ddd
bimaaghafara <bimaa7@gmail.com>
2022-05-12 10:11:59 +0700

    add axios

--- src/domains/home/index.tsx
+++ src/domains/home/index.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from 'react';
+import homeService from './service';
+
 export default function Home() {
+  useEffect(() => {
+    homeService
+      .getUsers()
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return <div>Homepage</div>;
 }
---
+++ src/services/http.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: {
+    'Content-type': 'application/json'
+  }
+});
+
+// Add a request interceptor
+http.interceptors.request.use((config) => {
+  // console.log(config)
+  // const token = localStorage.getItem('token');
+  // if (token) {
+  //   // add token to request headersif exist
+  //   config.headers['Authorization'] = `Bearer ${token}`;
+  // }
+  return config;
+});
+
+export default http;
