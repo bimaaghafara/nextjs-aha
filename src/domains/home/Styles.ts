a7721874b05269c7e0202907f19fa25017c167f2
bimaaghafara <bimaa7@gmail.com>
2022-05-16 17:56:38 +0700

    onprogress home page

--- src/domains/home/Styles.ts
+++ src/domains/home/Styles.ts
@@ -1,19 +1,22 @@
 import { SxObject } from 'src/types/SxObject';
 
+const isDesktop = `@media screen and (min-width: 601px)`;
+
 export const Styles = SxObject({
   root: {
-    '@media screen and (min-width: 601px)': {
+    [isDesktop]: {
       marginTop: '54px'
     }
   },
-  title: {
+  titleSearch: {
     color: '#fff',
     fontSize: '24px'
   },
   textfieldKeyword: {
     marginTop: '16px',
     '& input': {
-      color: '#fff'
+      color: '#fff',
+      height: '27px'
     },
     '& .MuiOutlinedInput-root': {
       '& fieldset': {
@@ -21,11 +24,73 @@ export const Styles = SxObject({
         borderRadius: '6px'
       },
       '&:hover fieldset': {
-        border: '3px solid #FF9B33'
+        border: '3px solid rgba(255, 255, 255, 0.5)'
       },
       '&.Mui-focused fieldset': {
         border: '3px solid #FF9B33'
       }
+    },
+    [isDesktop]: {
+      marginTop: '20px'
+    }
+  },
+  line: {
+    display: 'none',
+    [isDesktop]: {
+      display: 'block',
+      opacity: '0.1',
+      border: '1px solid #FFFFFF',
+      marginTop: '30px',
+      marginBottom: '2px'
+    }
+  },
+  titleResults: {
+    color: '#fff',
+    fontSize: '24px',
+    marginTop: '28px'
+  },
+  perPage: {
+    display: 'flex',
+    direction: 'row',
+    alignItems: 'flex-end',
+    marginTop: '16px',
+    color: '#fff',
+    [isDesktop]: {
+      marginTop: '20px'
+    }
+  },
+  perPageValue: {
+    fontSize: '48px',
+    lineHeight: '50px',
+    fontWeight: 700
+  },
+  perPageResults: {
+    fontSize: '16px',
+    letterSpacing: '0.15px',
+    paddingLeft: '10px',
+    paddingBottom: '4px'
+  },
+  buttonSearch: {
+    width: 'calc(100% - 40px)',
+    height: '40px',
+    boxShadow: 'none',
+    backgroundColor: '#fff',
+    fontWeight: 700,
+    border: '1px solid #121212',
+    color: '#121212',
+    position: 'fixed',
+    bottom: '66px',
+    marginBottom: '24px',
+    '&:hover': {
+      color: '#fff',
+      backgroundColor: '#121212',
+      border: '1px solid #fff',
+      boxShadow: 'none'
+    },
+    [isDesktop]: {
+      width: '343px',
+      bottom: 0,
+      marginBottom: '83px'
     }
   }
 });
--- src/domains/home/index.tsx
+++ src/domains/home/index.tsx
@@ -7,6 +7,7 @@ import PageLayout from 'src/components/PageLayout';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   useEffect(() => {
@@ -23,10 +24,24 @@ export default function Home() {
   return (
     <PageLayout activeMenu="home" withNotificationMenus={['tags']} isBottomNav>
       <Box sx={sx.root}>
-        <Typography component="h5" sx={sx.title}>
+        <Typography component="h5" sx={sx.titleSearch}>
           Search
         </Typography>
         <TextField placeholder="Keyword" variant="outlined" fullWidth sx={sx.textfieldKeyword} />
+        <Box sx={sx.line} />
+        <Typography component="h5" sx={sx.titleResults}>
+          # Of Results Per Page
+        </Typography>
+        <Box sx={sx.perPage}>
+          <Typography component="h3" sx={sx.perPageValue}>
+            30
+          </Typography>
+          <Typography sx={sx.perPageResults}>results</Typography>
+        </Box>
+
+        <Button variant="contained" sx={sx.buttonSearch}>
+          Search
+        </Button>
       </Box>
     </PageLayout>
   );
