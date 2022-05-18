89481e2622d37c48c4cd2668e8868f0f307a8294
bimaaghafara <bimaa7@gmail.com>
2022-05-18 14:36:20 +0700

    update: search page - skeleton

--- src/domains/Search/Styles.tsx
+++ src/domains/Search/Styles.tsx
@@ -3,6 +3,35 @@ import { SxObject } from 'src/types/SxObject';
 const isDesktop = `@media screen and (min-width: 601px)`;
 
 export const Styles = SxObject({
+  skeletonsContainer: {
+    marginTop: '-31px'
+  },
+  skeletonImg: {
+    background: '#333',
+    height: 0,
+    marginTop: '15px',
+    paddingTop: 'calc(100% * 222.67 / 335)',
+    transform: 'none',
+    [isDesktop]: {
+      paddingTop: 'calc(100% * 146 / 219)'
+    }
+  },
+  skeletonTitle: {
+    background: '#333',
+    height: '12px',
+    transform: 'none',
+    marginTop: '6.5px',
+    marginBottom: '3.5px',
+    width: '60%'
+  },
+  skeletonText: {
+    background: '#333',
+    height: '10px',
+    transform: 'none',
+    marginTop: '5px',
+    marginBottom: '2px',
+    width: '45%'
+  },
   topSection: {
     marginTop: '20px',
     marginBottom: '24px',
@@ -51,10 +80,19 @@ export const Styles = SxObject({
     marginTop: '39px',
     marginBottom: '24px',
     '&:hover': {
-      color: '#fff',
-      backgroundColor: '#121212',
+      color: '#fff !important',
+      backgroundColor: '#121212 !important',
       border: '1px solid #fff',
       boxShadow: 'none'
+    },
+    '&.MuiLoadingButton-loading': {
+      color: '#fff !important',
+      backgroundColor: '#121212 !important',
+      border: '1px solid #fff',
+      boxShadow: 'none'
+    },
+    '&..Mui-disabled': {
+      color: '#121212'
     },
     [isDesktop]: {
       width: '343px'
--- src/domains/Search/index.tsx
+++ src/domains/Search/index.tsx
@@ -7,6 +7,7 @@ import PageLayout from 'src/components/PageLayout';
 import Box from '@mui/material/Box';
 import LoadingButton from '@mui/lab/LoadingButton';
 import Grid from '@mui/material/Grid';
+import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
@@ -15,12 +16,26 @@ import { useGetInfiniteUsers } from './Services';
 
 export default function Search() {
   const router = useRouter();
+  const pageSize = Number(router.query.pageSize || 0);
   const { data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } =
     useGetInfiniteUsers(router.query, router.isReady);
 
   function renderContent() {
     if (error) return 'Error';
-    if (isLoading || !data) return 'Loading...';
+    if (isLoading || !data)
+      return (
+        <Grid container spacing={2} sx={sx.skeletonsContainer}>
+          {Array(pageSize > 15 ? 15 : pageSize)
+            .fill(1)
+            .map((e, i) => (
+              <Grid key={e * i} item xs={12} sm={4}>
+                <Skeleton sx={sx.skeletonImg} />
+                <Skeleton sx={sx.skeletonTitle} />
+                <Skeleton sx={sx.skeletonText} />
+              </Grid>
+            ))}
+        </Grid>
+      );
     return (
       <>
         <Grid container spacing={2}>
@@ -33,13 +48,13 @@ export default function Search() {
           )}
         </Grid>
         <LoadingButton
-          onClick={() => {
-            if (hasNextPage) fetchNextPage();
-          }}
+          onClick={() => fetchNextPage()}
           loading={isFetchingNextPage}
           loadingPosition="end"
+          endIcon={<></>}
           variant="outlined"
           sx={sx.buttonMore}
+          disabled={!hasNextPage}
         >
           {hasNextPage ? 'MORE' : 'Nothing more to load'}
         </LoadingButton>
